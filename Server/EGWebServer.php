6641c6115a3bb6409aa7c14ce07595578a970ab7
mingtingjian <mingtingjian@sina.com>
2015-06-26 17:51:23 +0800

    删除setConfig方法

--- Server/EGWebServer.php
+++ Server/EGWebServer.php
@@ -12,25 +12,6 @@ class EGWebServer extends EGBaseServer{
 	public function setWebRoot($webRoot){
 		$this->_webRoot=$webRoot;
 	}
-	
-	public function loadConfig($config) {
-		// TODO Auto-generated method stub
-		if (empty($config)){
-			$config=array(
-					//工作进程
-					'worker_num' => 4,
-					//是否守护进程
-					'daemonize' => false,
-					//最大请求数
-					'max_request' => 10000,
-					//工作模式
-					'dispatch_mode' => 1
-			);
-		}
-		$this->_config=$config;
-	
-		$this->_server->set($config);
-	}
 	/**
 	 * WEB服务器回调
 	 * @param \swoole_http_request $request
